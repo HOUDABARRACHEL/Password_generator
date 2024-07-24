2e056388e5383a26fd604aff57219d3975e9d453
barrachel_houda <houdabarrachel@gmail.com>
2024-07-24 01:54:34 +0100

    Bibliotheques

---
+++ Generator.py
@@ -0,0 +1,5 @@
+import sys #Cette bibliothèque permet d'interagir avec l'interpréteur Python
+import string # Fournit des constantes et des fonctions pour manipuler des chaînes de caractères
+import random #  Générer des nombres aléatoires pour diverses applications
+import math #Fournit des fonctions mathématiques
+import argparse # Gérer les arguments de ligne de commande
