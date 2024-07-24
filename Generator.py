5f5d4a9c20bb5b0636633d1512143b7f761c8569
barrachel_houda <houdabarrachel@gmail.com>
2024-07-24 01:57:20 +0100

    First function with explication

--- Generator.py
+++ Generator.py
@@ -3,3 +3,12 @@ import string # Fournit des constantes et des fonctions pour manipuler des chaî
 import random #  Générer des nombres aléatoires pour diverses applications
 import math #Fournit des fonctions mathématiques
 import argparse # Gérer les arguments de ligne de commande
+
+def main():
+    args = parse_args() #Appelle une fonction parse_args qui analyse et retourne les arguments de ligne de commande.
+
+    if args is None: # Verfie si args est none
+        interactive() #Appelle une fonction nommée interactive
+    else:
+        automatic(args) #Si des arguments de ligne de commande ont été fournis, le programme doit fonctionner en mode automatique.
+#génère des mots de passe basés sur les paramètres passés en arguments  depuis la ligne de commande.
